dc53558b6464a733331b7bc76a3b04ca7071288b
abraxasrex <jbreasor@gmail.com>
2016-06-29 17:15:42 -0700

    first

---
+++ server.js
@@ -0,0 +1,21 @@
+
+var express  = require('express');
+var app     = express();
+//var mongoose = require('mongoose');
+var morgan = require('morgan');
+//var bodyParser = require('body-parser');
+//var methodOverride = require('method-override');
+
+app.use(express.static(__dirname + '/vrBreakfast'));  //set the static file name to public ...on top of current dir name?)
+app.use(morgan('dev')); //logs to dev console
+//app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
+//app.use(bodyParser.json());                                     // parse application/json
+//app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+//app.use(methodOverride());
+
+app.get('*', function (req, res) {
+  res.sendfile('./vrBreakfast/index.html');
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port);
---
+++ todo.md
@@ -0,0 +1,18 @@
+Todo:  
+
+1. UX:
+  - generate random sausages, speed up as levels increase
+  - sausage catch collision event  
+  - win/loss tracking  
+  - db for scores  
+
+2. Textures:
+  - Donut texture  
+  - breakfast table texture  
+
+3. Lighting:
+  - Breakfast appropriate lighting.  
+
+4. Stretch  
+ - User movement  
+ - different foods for different levels  
