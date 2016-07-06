666711aa9a8a75d4142ad154c3688c9ffef4244c
abraxasrex <jbreasor@gmail.com>
2016-07-06 15:59:38 -0700

    replaced sausages with less disgusting oranges

--- public/js/vr.js
+++ public/js/vr.js
@@ -1,29 +1,30 @@
 var scene,
   camera,
-  cloneCam,
   renderer,
-  material,
   donut,
-  sausageMaterial,
+  enemyMaterial,
   hud,
-  hudText,
   element,
   container,
   effect,
   controls,
   clock;
-var sausages = [];
+var enemies = [];
 var score = 0;
 var health = 10;
 var playRadius = 10;
-var sausageRate = 4000;
+var enemyInterval = 4000;
+var enemyDropRate = .1;
 var level = 1;
+var currentGame;
+var donutHeight = 12.5;
+var hudHeight = 37.5;
 
 //palette
 var sunlight = new THREE.Color('rgb(255, 255, 102)');
-var yellowText = new THREE.Color('rgb(255, 255, 0)');
 var white = new THREE.Color('rgb(100,100,100)');
 var orangeText = new THREE.Color('rgb(255, 102, 0)');
+var boldYellow = new THREE.Color('rgb(255, 255, 0)');
 
 // math helpers
 function randomDegree(){
@@ -42,7 +43,6 @@ THREE.Utils = {
     return vector;
   }
 };
-
 // device orientation controls
 function setOrientationControls(e) {
   if (!e.alpha) {
@@ -51,7 +51,6 @@ function setOrientationControls(e) {
   controls = new THREE.DeviceOrientationControls(camera, true);
   controls.connect();
   controls.update();
-
   element.addEventListener('click', fullscreen, false);
   window.removeEventListener('deviceorientation', setOrientationControls, true);
 }
@@ -61,21 +60,22 @@ function animate() {
   update(clock.getDelta());
   render(clock.getDelta());
 
- // reorient donut and hud
-  donut.position.x = THREE.Utils.cameraLookDir(camera).x;
-  donut.position.z = THREE.Utils.cameraLookDir(camera).z;
-  hud.position.x = THREE.Utils.cameraLookDir(camera).x * 10;
-  hud.position.z = THREE.Utils.cameraLookDir(camera).z * 10;
+ // reorient donut and hud to camera
+  donut.position.set(THREE.Utils.cameraLookDir(camera).x, donutHeight, THREE.Utils.cameraLookDir(camera).z);
+  hud.position.set(THREE.Utils.cameraLookDir(camera).x * 10, hudHeight, THREE.Utils.cameraLookDir(camera).z * 10);
   hud.lookAt(donut.position);
 
-  for(var i = 0; i < sausages.length; i++){
-    processSausage(sausages[i]);
+  for(var i = 0; i < enemies.length; i++){
+    handleEnemies(enemies[i]);
   }
 }
 
+function createHudString(){
+  return 'score: ' + score + '   hp: ' + health;
+}
+
 function hudChange(){
-  hudText = 'score: ' + score + '   health: ' + health;
-  hud.geometry = new THREE.TextGeometry(hudText, {size:7.5, height:1});
+  hud.geometry = new THREE.TextGeometry(createHudString(), {size:7.5, height:1});
 }
 
 function resize() {
@@ -115,8 +115,6 @@ function init() {
   camera = new THREE.PerspectiveCamera(90, window.innerWidth / window.innerHeight, 0.001, 700);
   camera.position.set(0, 15, 0);
   scene.add(camera);
-  cloneCam = camera.clone();
-  cloneCam.position.x = -1;
   renderer = new THREE.WebGLRenderer();
   element = renderer.domElement;
   renderer.setClearColor( sunlight, 0);
@@ -124,24 +122,24 @@ function init() {
   container.appendChild(element);
 
   // init donut torus
-  geometry = new THREE.TorusGeometry( 2.5, 0.75, 100, 100);
+  var donutGeometry = new THREE.TorusGeometry( 2.5, 0.75, 100, 100);
   var donutTexture = THREE.ImageUtils.loadTexture('textures/donut.jpg');
-  donutTexture.anisotropy = renderer.getMaxAnisotropy();
-  material = new THREE.MeshPhongMaterial({
+  var material = new THREE.MeshLambertMaterial({
     shading: THREE.FlatShading,
+    wrapT: THREE.RepeatWrapping,
+    wrapS:THREE.RepeatWrapping,
     map: donutTexture
   });
-  donut = new THREE.Mesh( geometry, material );
+  donut = new THREE.Mesh( donutGeometry, material );
   donut.position.set(0,12.5,5);
   donut.rotation.x = -1;
   scene.add(donut);
 
-  //sausage texture
-  var sausageTexture = THREE.ImageUtils.loadTexture('textures/sausage.jpg');
-  sausageTexture.anisotropy = renderer.getMaxAnisotropy();
-  sausageMaterial = new THREE.MeshPhongMaterial({
+  //enemy texture
+  var enemyTexture = THREE.ImageUtils.loadTexture('textures/orange.jpg');
+  enemyMaterial = new THREE.MeshPhongMaterial({
     shading: THREE.FlatShading,
-    map: sausageTexture
+    map: enemyTexture
   });
 
   // stereo vision
@@ -149,11 +147,7 @@ function init() {
 
   // controls fallback
   controls = new THREE.OrbitControls(camera, element);
-  controls.target.set(
-    camera.position.x + 0.15,
-    camera.position.y,
-    camera.position.z
-  );
+  controls.target.set(camera.position.x + 0.15, camera.position.y, camera.position.z);
   controls.noPan = true;
   controls.noZoom = true;
 
@@ -164,7 +158,7 @@ function init() {
   var light = new THREE.PointLight(boldYellow, 2, 100);
   light.position.set(50, 50, 50);
   scene.add(light);
-  var boldYellow = new THREE.Color('rgb(255, 255, 0)');
+
   var lightScene = new THREE.PointLight(white, 2, 100);
   lightScene.position.set(0, 5, 0);
   scene.add(lightScene);
@@ -181,8 +175,7 @@ function init() {
   scene.add(floor);
 
   // create HUD
-  hudText = 'score: ' + score + '   health: ' + health;
-  var textObj = new THREE.TextGeometry(hudText, {size:7.5, height:1});
+  var textObj = new THREE.TextGeometry(createHudString(), {size:7.5, height:1});
   var hudMaterial = new THREE.MeshBasicMaterial({ color: orangeText });
   hud = new THREE.Mesh( textObj, hudMaterial );
   scene.add(hud);
@@ -193,59 +186,56 @@ function init() {
   animate();
 }
 
-function dropSausages(){
-  var geometry = new THREE.CylinderGeometry(1, 1, 5, 24);
-  var newSausage = new THREE.Mesh( geometry, sausageMaterial );
+function dropEnemy(){
+  var geometry = new THREE.SphereGeometry(1, 32, 32);
+  var newEnemy = new THREE.Mesh( geometry, enemyMaterial );
   var radian = randomRadian();
-  newSausage.position.set(playRadius * Math.cos(radian), 37.5, playRadius * Math.sin(radian));
-  sausages.push(newSausage);
-  scene.add(newSausage);
+  newEnemy.position.set(playRadius * Math.cos(radian), 37.5, playRadius * Math.sin(radian));
+  enemies.push(newEnemy);
+  scene.add(newEnemy);
 }
 
 function winLevel(){
   var parsed = parseInt(level) + 1;
-  hudText = 'you beat level ' + level + ' ' + 'level ' + parsed + ' incoming';
+  var hudText = 'level ' + parsed;
   hud.geometry = new THREE.TextGeometry(hudText, {size:7.5, height:1});
   level += 1;
 
-  currentGame.clearInterval();
-  sausages.forEach(function(sausage){
-    scene.remove(sausage);
+  window.clearInterval(currentGame);
+  enemies.forEach(function(enemy){
+    scene.remove(enemy);
   });
-  sausages = [];
+  enemies = [];
   setTimeout(nextLevel, 2000);
 }
 
 function nextLevel(){
-  sausageRate = sausageRate / 1.5;
+  enemyInterval = enemyInterval / 1.5;
+  enemyDropRate += 0.025;
   health = 10;
-  hudText = 'score: ' + score + '   health: ' + health;
-  hud.geometry = new THREE.TextGeometry(hudText, {size:7.5, height:1});
-
-  currentGame = setInterval(dropSausages, sausageRate);
+  hud.geometry = new THREE.TextGeometry(createHudString(), {size:7.5, height:1});
+  currentGame = setInterval(dropEnemy, enemyInterval);
 }
 
-function processSausage(sausage){
-  if(score >= (15 * level)){
+function handleEnemies(enemy){
+  if(score >= (5 * level)){
     winLevel();
     return;
   }
-  sausage.position.y -= .1;
-  if(sausage.position.y < -2.5){
-    sausages.splice(sausages[sausage], 1);
-    //scene.children.splice(scene.children.indexOf(sausage), 1);
-    scene.remove(sausage);
+  enemy.position.y -= enemyDropRate;
+  if(enemy.position.y < -2.5){
+    enemies.splice(enemies[enemy], 1);
+    scene.remove(enemy);
     health -= 1;
     hudChange();
   }
-  if(donut.position.distanceTo(sausage.position) < 4){
-    sausages.splice(sausages[sausage], 1);
-  //  scene.children.splice(scene.children.indexOf(sausage), 1);
-    scene.remove(sausage);
+  if(donut.position.distanceTo(enemy.position) < 2){
+    enemies.splice(enemies[enemy], 1);
+    scene.remove(enemy);
     score += 1;
     hudChange();
   }
 }
 
 init();
-currentGame = setInterval(dropSausages, sausageRate);
+currentGame = setInterval(dropEnemy, enemyInterval);
